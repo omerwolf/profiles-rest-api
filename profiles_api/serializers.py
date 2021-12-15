96564fae76b2adc9060afc51dbdc725f0175a538
omerwolf <omer2wolf@gmail.com>
2021-12-15 23:09:41 +0200

    Added POST, PUT, PATCH, DELETE methods

---
+++ profiles_api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+
+class HelloSerializers(serializers.Serializer):
+    """Serializers a name filed for testing our APIView"""
+    name = serializers.CharField(max_length=10)
